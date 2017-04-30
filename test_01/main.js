e22669ceca1216a9c2b77638bbbfd1cac9224966
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-04-30 16:33:47 +0900

    問1を作成

---
+++ test_01/main.js
@@ -0,0 +1,92 @@
+/**
+ *	skill check test main.js
+ * */
+/* 第1問 */
+// 定数
+const PermissionKeyCodes = [8, 9, 13, // 8 = BackSpace, 9 = Tab, 13 = enter
+                            37, 39, 46, // 37 = 左キー, 39 = 右キー, 46 = delete
+                            48, 49, 50, 51, 52, // 数字キー = 1 〜 9
+                            53, 54, 55, 56, 57,
+                            96, 97, 98, 99, 100,  // 数字キー(テンキー) = 1 〜 9
+                            101, 102, 103, 104, 105,
+                            110, 190];  // 110 = ピリオド(テンキー), 190 = ピリオド
+// 変数
+let selectedMode = 0;   // 計算モード
+let calcResult;     // 計算結果
+let inputLeft;      // 左辺に入力した値
+let inputRight;     // 右辺に入力した値
+let result;         // 計算結果
+/**
+* 計算方法を変更する関数
+*/
+function clickSelectedCalcMode(obj){
+  // 選択しているボタンの選択表示classを削除
+  document.getElementsByClassName('calcModeButton')[selectedMode].classList.remove('selectedMode');
+  // 計算方法を変更
+  selectedMode = obj.value;
+  // 選択したボタンに選択表示classを追加
+  document.getElementsByClassName('calcModeButton')[selectedMode].classList.add('selectedMode');
+  // id="calcMode"のtextを変更
+  if(selectedMode == 0){
+    document.getElementById('calcMode').innerHTML = '+';
+  }else if(selectedMode == 1){
+    document.getElementById('calcMode').innerHTML = '-';
+  }else if(selectedMode == 2){
+    document.getElementById('calcMode').innerHTML = '×';
+  }else if(selectedMode == 3){
+    document.getElementById('calcMode').innerHTML = '÷';
+  }
+}
+/**
+* 入力された時の関数
+*/
+function keyDown(event){
+  let flag = false;
+
+  for(let i = 0, length = PermissionKeyCodes.length; i<length; i++){
+    if(event.keyCode == PermissionKeyCodes[i]){
+      flag = true;
+      break;
+    }
+  }
+
+  if(!flag){
+    // 入力イベントをキャンセルする
+    event.preventDefault();
+  }
+
+}
+
+/**
+* 計算ボタンが押されたときの関数
+*/
+function executeCalc(){
+  // 入力された値を取得する
+  inputLeft = parseFloat(document.getElementById('calcLeft').value);
+  inputRight = parseFloat(document.getElementById('calcRight').value);
+
+  // 入力チェック
+  if(!inputLeft || !inputRight){
+    alert('入力値が不正です。');
+    return false;
+  }
+  // 計算
+  calc();
+
+  // 計算結果を表示
+  document.getElementById('calcResult').innerHTML = result;
+}
+/**
+* 計算ボタンが押されたときの関数
+*/
+function calc(){
+  if(selectedMode == 0){
+    result = Math.ceil((inputLeft + inputRight)*100)/100;
+  }else if(selectedMode == 1){
+    result = Math.ceil((inputLeft - inputRight)*100)/100;
+  }else if(selectedMode == 2){
+    result = Math.ceil((inputLeft * inputRight)*100)/100;
+  }else if(selectedMode == 3){
+    result = Math.ceil((inputLeft / inputRight)*100)/100;
+  }
+}
