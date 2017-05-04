97dc26ec6974a36083aa78db2717b057a860f3db
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-05-04 23:30:26 +0900

    問題編を作成。テストしやすいように、masterを問題にしています

--- test_01/main.js
+++ test_01/main.js
@@ -1,65 +1,58 @@
 /**
  *	skill check test main.js
- *  回答編
+ *  問題編
  */
 
 /**
  * 第1問 スコープ問題
- * console.logで出力される結果を、選択してください
+ * console.logの出力結果を、選択してください
+ * 答えがわからないようにコメント化しています。
  */
 
 // (1)
 {
 	var scope1 = "scope1";
 }
-console.log('1-1')
 //console.log(scope1);
 // 選択
 // 1. ReferenceError
 // 2. scope1
-// 正解：2
 
 // (2)
 {
 	let scope2 = "scope2";
 }
-console.log('1-2');
 // console.log(scope2);
 // 選択
 // 1. ReferenceError
 // 2. scope2
-// 正解：1
 
 // (3)
 {
 	const scope3 = "scope3";
 }
-console.log('1-3');
 //console.log(scope3)
 // 選択
 // 1. ReferenceErrorとなる
 // 2. scope3
-// 正解：1
 
 // (4)
 function fun4(){
 	var scope4 = "scope4";
 	return scope4;
 }
-console.log('1-4');
 //console.log(scope4);
 // 選択
 // 1. ReferenceError
 // 2. scope4
-// 正解：1
 
 /**
  * 第2問 関数定義
- * console.logで出力される結果を、選択してください
+ * console.logの出力結果を、選択してください
+ * 答えがわからないようにコメント化しています。
  */
 
 // (1)
-console.log('2-1');
 //console.log(fun5());
 let fun5 = function(){
 	return 'fun5';
@@ -67,10 +60,8 @@ let fun5 = function(){
 // 選択
 // 1. ReferenceError
 // 2. fun5
-// 正解：1
 
 // (2)
-console.log('2-2');
 //console.log(fun6());
 function fun6(){
 	return 'fun6';
@@ -78,21 +69,18 @@ function fun6(){
 // 選択
 // 1. ReferenceError
 // 2. fun6
-// 正解：2
 
 /**
  * 第3問 ajax(非同期通信)
  * この処理はバグがあります。どこが原因なのか指摘してください。
  */
-/**
- * 解答例
- * doneの中でtbodyの更新処理を行えばよい。
- */
 
 $(function(){
 	const $tbody = $('#tbody');
 	let html = '';
+	let data = null;
 
+	// 非同期通信
 	$('#ajaxBtn').on('click', function(){
 		$.ajax({
 			method : 'GET',
@@ -100,14 +88,17 @@ $(function(){
 			dataType : 'json',
 			timeout : 5000,
 		}).done(function(arr_data){
-			arr_data.forEach(function(value, index, array){
-				html += createTRow(value);
-			});
-			$tbody.append(html);
+			data = arr_data;
 		}).fail(function(){
 			alert('ajax error!');
 		});
 	});
+
+	// テーブルに反映
+	arr_data.forEach(function(value, index, array){
+		html += createTRow(value);
+	});
+	$tbody.append(html);
 });
 
 function createTRow(rowData){
@@ -126,12 +117,6 @@ function createTRow(rowData){
  * 改善点など、突っ込みどころをあげてください。
  */
 
-/**
- * 解答例
- * ・グローバルで定数や変数を宣言している必要性は？ローカル変数にできないか考慮する必要がある
- * 　-> 即時関数でラップして、名前空間汚染の防止
- */
-
 // 定数
 const PermissionKeyCodes = [8, 9, 13, // 8 = BackSpace, 9 = Tab, 13 = enter
                             37, 39, 46, // 37 = 左キー, 39 = 右キー, 46 = delete
@@ -147,15 +132,6 @@ let inputLeft;      // 左辺に入力した値
 let inputRight;     // 右辺に入力した値
 let result;         // 計算結果
 
-/**
-* 解答例(定数、変数、htmlの記述)
-* ・引数objが何かわかりにくい
-* 　-> わかりやすい引数名に変更。又は、コメントで補足する
-* ・document.getElementById(id名)の記述回数が多い
-* 　-> 処理前に、変数化して記述回数を減らす。できれば、関数の外で変数宣言(即時関数のローカル変数)できればよい
-* ・if文が不適切
-* 　-> == は値のみでデータ型では判断されない。===を使用するほうが望ましい
-*/
 /**
 * 計算方法を変更する関数
 */
@@ -178,14 +154,6 @@ function clickSelectedCalcMode(obj){
   }
 }
 
-
-/**
-* 解答例
-* ・htmlでonclick属性を使用はやめる
-* 　-> addEventListenerを使用して、jsファイルのみでJavaScriptのコードを管理する
-* ・if文が不適切
-* 　-> == は値のみでデータ型では判断されない。===を使用するほうが望ましい
-*/
 /**
 * 数値が入力された時の関数
 */
@@ -205,15 +173,7 @@ function keyDown(event){
   }
 
 }
-/**
-* 解答例
-* ・htmlでonclick属性を使用はやめる
-* 　-> addEventListenerを使用して、jsファイルのみでJavaScriptのコードを管理する
-* ・0を入力した場合、不正判定される
-* 　-> parseFloat()する前に、入力チェックする
-* ・document.getElementById('calcLeft')、document.getElementById('calcRight')を変数に
-* 　-> 上の処理でも呼ばれているので、変数化して呼ぶ回数を減らす
-*/
+
 /**
 * 計算ボタンが押されたときの関数
 */
@@ -233,15 +193,7 @@ function executeCalc(){
   // 計算結果を表示
   document.getElementById('calcResult').innerHTML = result;
 }
-/**
-* 解答例
-* ・if文が不適切
-* 　-> == は値のみでデータ型では判断されない。===を使用するほうが望ましい
-* ・引数とreturnを使用する
-* 　-> グローバル変数を定義しなくてもいい
-* ・切り上げ処理が不適切(※結構見落とす所)
-* 　-> 1 + 0.1 の結果が1.11となる。0.1を表現する場合の誤差が考慮されていない。
-*/
+
 /**
 * 計算ボタンが押されたときの関数
 * 少数は第2位まで表示。端数は切り上げる
