d03c0f898861d2a44e811dea6468c68ede2e1905
ikkyu-3 <toru.hasegawa.0521@gmail.com>
2017-05-04 18:49:10 +0900

    テスト内容を変更。回答編から作成中

--- test_01/main.js
+++ test_01/main.js
@@ -1,7 +1,117 @@
 /**
  *	skill check test main.js
- * */
-/* 第1問 */
+ *  回答編
+ */
+
+/**
+ * 第1問 スコープ問題
+ * console.logで出力される結果を、選択してください
+ */
+
+// (1)
+{
+	var scope1 = "scope1";
+}
+console.log('1-1')
+//console.log(scope1);
+// 選択
+// 1. ReferenceError
+// 2. scope1
+// 正解：2
+
+// (2)
+{
+	let scope2 = "scope2";
+}
+console.log('1-2');
+// console.log(scope2);
+// 選択
+// 1. ReferenceError
+// 2. scope2
+// 正解：1
+
+// (3)
+{
+	const scope3 = "scope3";
+}
+console.log('1-3');
+//console.log(scope3)
+// 選択
+// 1. ReferenceErrorとなる
+// 2. scope3
+// 正解：1
+
+// (4)
+function fun4(){
+	var scope4 = "scope4";
+	return scope4;
+}
+console.log('1-4');
+//console.log(scope4);
+// 選択
+// 1. ReferenceError
+// 2. scope4
+// 正解：1
+
+/**
+ * 第2問 関数定義
+ * console.logで出力される結果を、選択してください
+ */
+
+// (1)
+console.log('2-1');
+//console.log(fun5());
+let fun5 = function(){
+	return 'fun5';
+};
+// 選択
+// 1. ReferenceError
+// 2. fun5
+// 正解：1
+
+// (2)
+console.log('2-2');
+//console.log(fun6());
+function fun6(){
+	return 'fun6';
+}
+// 選択
+// 1. ReferenceError
+// 2. fun6
+// 正解：2
+
+/**
+ * 第3問 ajax(非同期通信)
+ * この処理はバグがあります。どこが原因なのか指摘してください。という予定
+ */
+$(function(){
+	$('#ajaxBtn').on('click', function(){
+		$.ajax({
+			method : 'GET',
+			url : 'data.json',
+			dataType : 'json',
+			timeout : 5000,
+		}).done(function(arr_data){
+			arr_data.forEach(function(value, index, array){
+				$('#tbody').append(createTRow(value));
+			});
+		}).fail(function(){
+			alert('ajax error!');
+		});
+	});
+});
+
+function createTRow(rowData){
+	return `<tr>` +
+						`<td>${rowData.title}</td>` +
+						`<td>${rowData.author}</td>` +
+						`<td>${rowData.price}</td>` +
+						`<td>${rowData.isbn}</td>` +
+						`<td>${rowData.publish}</td>` +
+						`<td>${rowData.published}</td>` +
+					`</tr>`;
+}
+
 // 定数
 const PermissionKeyCodes = [8, 9, 13, // 8 = BackSpace, 9 = Tab, 13 = enter
                             37, 39, 46, // 37 = 左キー, 39 = 右キー, 46 = delete
@@ -90,24 +200,3 @@ function calc(){
     result = Math.ceil((inputLeft / inputRight)*100)/100;
   }
 }
-
-/* 第2問 */
-window.onload = function(){
-  /* クリックイベントを登録 */
-  // 「新規登録」ボタンがクリックされた時の関数
-  document.getElementById('new_user').addEventListener('click', function(e){
-    e.preventDefault();
-    document.getElementById('dialog').style.display = "block";
-  });
-  // 「送信」ボタンがクリックされた時の関数
-  document.getElementById('submit').addEventListener('click', function(e){
-    e.preventDefault();
-    const form = document.getElementById('form');
-    console.log(form);
-  });
-  // 「キャンセル」ボタンがクリックされた時の関数
-  document.getElementById('cancel').addEventListener('click', function(e){
-    e.preventDefault();
-    document.getElementById('dialog').style.display = "none";
-  });
-};
